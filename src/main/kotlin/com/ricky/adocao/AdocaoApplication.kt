da9a006106f51887bae0f55a62f3cb80e027a3cc
Luíz Henrique de Carvalho Teles <rlyowlly19@gmail.com>
2024-02-27 08:50:40 -0300

    projeto vazio

---
+++ src/main/kotlin/com/ricky/adocao/AdocaoApplication.kt
@@ -0,0 +1,11 @@
+package com.ricky.adocao
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AdocaoApplication
+
+fun main(args: Array<String>) {
+	runApplication<AdocaoApplication>(*args)
+}
---
+++ src/test/kotlin/com/ricky/adocao/AdocaoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ricky.adocao
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AdocaoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
