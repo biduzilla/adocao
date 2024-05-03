08359f933734e8815d31bb6f8be63dc290f75d1e
Luíz Henrique de Carvalho Teles <rlyowlly19@gmail.com>
2024-05-03 09:44:05 -0300

    dagger

---
+++ app/app/src/main/java/com/ricky/adocaoapp/AdocaoApplication.kt
@@ -0,0 +1,8 @@
+package com.ricky.adocaoapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AdocaoApplication:Application() {
+}
---
+++ app/app/src/main/java/com/ricky/adocaoapp/di/AppModule.kt
@@ -0,0 +1,34 @@
+package com.ricky.adocaoapp.di
+
+import com.ricky.adocaoapp.data.network.UserAPI
+import com.ricky.adocaoapp.data.repository.UserRepositoryImpl
+import com.ricky.adocaoapp.domain.repository.UserRepository
+import com.ricky.adocaoapp.utils.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun proviteUserApi(): UserAPI {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(UserAPI::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideUserRepository(
+        api: UserAPI
+    ): UserRepository = UserRepositoryImpl(api)
+}
