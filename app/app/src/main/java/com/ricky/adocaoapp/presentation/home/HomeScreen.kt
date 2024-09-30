24c0d41b586c0348141ccb0b5238ab5db054e38d
Luíz Henrique de Carvalho Teles <rlyowlly19@gmail.com>
2024-09-30 08:38:01 -0300

    home screen fb

--- app/app/src/main/java/com/ricky/adocaoapp/presentation/home/HomeScreen.kt
+++ app/app/src/main/java/com/ricky/adocaoapp/presentation/home/HomeScreen.kt
@@ -2,6 +2,10 @@ package com.ricky.adocaoapp.presentation.home
 
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -19,6 +23,14 @@ fun HomeScreen(
                 search = state.search,
                 onChangePesquisa = { onEvent(HomeEvent.OnChangePesquisa(it)) },
                 onChangeFiltro = { onEvent(HomeEvent.OnChangeFiltro(it)) })
+        },
+        floatingActionButton = {
+            FloatingActionButton(onClick = {}) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = null
+                )
+            }
         }
     ) { paddingValues ->
         LazyColumn(modifier = Modifier.padding(paddingValues)) {
